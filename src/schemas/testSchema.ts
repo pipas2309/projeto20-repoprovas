d89a45b43fb60a7dd2cdaad210ba152fd02d5de3
Lucas Palharini <lucas230991@hotmail.com>
2022-09-19 13:52:38 -0300

    fix: fixed missing id in test schema

--- src/schemas/testSchema.ts
+++ src/schemas/testSchema.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const newTest = Joi.object({
     name: Joi.string().min(1).max(50).required(),
     pdfUrl: Joi.string().uri().required(),
-    category: Joi.string().required(),
+    categoryId: Joi.string().required(),
     teacherId: Joi.number().required(),
     disciplineId: Joi.number().required()
 });
