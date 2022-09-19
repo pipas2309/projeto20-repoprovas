c9d5b2739cda41b784904e6444359315c1f5bbe6
Lucas Palharini <lucas230991@hotmail.com>
2022-09-19 13:31:23 -0300

    feat: test schema created

---
+++ src/schemas/testSchema.ts
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+export const newTest = Joi.object({
+    name: Joi.string().min(1).max(50).required(),
+    pdfUrl: Joi.string().uri().required(),
+    category: Joi.string().required(),
+    teacherId: Joi.number().required(),
+    disciplineId: Joi.number().required()
+});
