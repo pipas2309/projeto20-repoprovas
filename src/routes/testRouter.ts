4b36c05893520ee99248974abdbb2a4666c642e6
Lucas Palharini <lucas230991@hotmail.com>
2022-09-19 14:54:54 -0300

    feat: GET/ router (by discipline) created

--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -3,9 +3,12 @@ import { Router } from "express";
 import { testController } from "../controllers";
 
 import schemaValidator from "../middlewares/schemaValidatorMiddleware";
+import tokenValidator from "../middlewares/tokenValidatorMiddleware";
 
 const router = Router();
 
-router.post("/test", schemaValidator('newTest'), testController.newTest);
+router.post("/test", tokenValidator ,schemaValidator('newTest'), testController.newTest);
+//router.get("/test/discipline/:id", tokenValidator, testController.getByDiscipline)
+
 
 export default router;
