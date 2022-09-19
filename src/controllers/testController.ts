8b9c998a8d7302e73d60e0730523a93fdca7abaa
Lucas Palharini <lucas230991@hotmail.com>
2022-09-19 13:40:54 -0300

    feat: test controller created

---
+++ src/controllers/testController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+//import { ITestSimple } from '../interfaces/testInterface';
+
+//import { testService } from '../services';
+
+
+export async function newTest(req: Request, res: Response) {
+    // const { name, pdfUrl, category, teacherId, disciplineId }: ITestSimple = req.body;
+
+    // const response: string = await testService.register(name, pdfUrl, category, teacherId, disciplineId);
+
+    // res.status(202).send(response);
+}
